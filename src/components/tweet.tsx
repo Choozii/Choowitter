1aaa0b19570ca195863e5e703b102e711728bab2
Choozii <jiyechoo@gmail.com>
2024-05-06 15:57:47 +0900

    feat: 삭제 기능 추가

--- src/components/tweet.tsx
+++ src/components/tweet.tsx
@@ -1,5 +1,8 @@
 import styled from "styled-components";
 import { ITweet } from "./timeline";
+import { auth, db, storage } from "../firebase";
+import { collection, deleteDoc, doc } from "firebase/firestore";
+import { deleteObject, ref } from "firebase/storage";
 
 const Wrapper = styled.div`
   display: grid;
@@ -25,12 +28,53 @@ const Payload = styled.p`
   font-size: 18px;
 `;
 
-const Tweet = ({ userName, photo, tweet }: ITweet) => {
+const DeleteButton = styled.button`
+  background-color: tomato;
+  color: white;
+  font-weight: 600;
+  border: 0;
+  font-size: 12px;
+  padding: 5px 10px;
+  text-transform: uppercase;
+  border-radius: 5px;
+  cursor: pointer;
+`;
+
+const Tweet = ({ userName, photo, tweet, userId, id }: ITweet) => {
+  const user = auth.currentUser;
+  const onDelete = async () => {
+    const ok = confirm("Are you sure you want to delete this tweet?");
+
+    if (!ok) {
+      return;
+    }
+
+    if (user?.uid !== userId) {
+      return;
+    }
+
+    try {
+      await deleteDoc(doc(db, "tweets", id));
+      if (photo) {
+        const photoRef = ref(
+          storage,
+          `tweets/${user.uid}-${user.displayName}/${id}`,
+        );
+        await deleteObject(photoRef);
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
   return (
     <Wrapper>
       <Column>
         <Username>{userName}</Username>
         <Payload>{tweet}</Payload>
+        {user?.uid === userId ? (
+          <DeleteButton onClick={onDelete}>Delete</DeleteButton>
+        ) : null}
       </Column>
       {photo ? (
         <Column>
