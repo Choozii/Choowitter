82e552e53f18b7db169b6d16a890b908832ac61f
Choozii <jiyechoo@gmail.com>
2024-05-06 15:20:57 +0900

    feat: firebase 데이터 조회

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,12 +1,19 @@
 import PostTwitForm from "../components/post-twit-form";
 import styled from "styled-components";
+import Timeline from "../components/timeline";
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+  display : grid;
+  gap : 50px;
+  overflow-y ; scroll;
+  grid-template-rows : 1fr 5fr;
+`;
 
 const Home = () => {
   return (
     <Wrapper>
       <PostTwitForm />
+      <Timeline />
     </Wrapper>
   );
 };
